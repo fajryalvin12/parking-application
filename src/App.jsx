c3fdf70c56489bf86f03e24c5570153e372a2b1b
fajryalvin12 <fajryalvin12@gmail.com>
2024-11-30 17:05:12 +0700

    initiate new project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-blue-500 flex flex-col justify-center items-center h-screen">
+        Hello
+      </h1>
+    </>
+  );
+}
+
+export default App;
